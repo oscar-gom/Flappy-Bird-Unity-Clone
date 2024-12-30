0b2a46063fa8d4a924e16d4aecb12046ed9e725d
Óscar Gómez <oscargomezsedas@gmail.com>
2024-12-30 19:28:47 +0100

    coded flap

--- Assets/Script/PlayerFlap.cs
+++ Assets/Script/PlayerFlap.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class PlayerFlap : MonoBehaviour
 {
+    public float force = 10.0f;
+    private Rigidbody2D _rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ApplyImpulse();
+        }
+    }
+    
+    void ApplyImpulse()
+    {
+        float originalGravity = _rb.gravityScale;
         
+        _rb.gravityScale = 0;
+        _rb.linearVelocity = Vector2.up * force;
+        
+        _rb.gravityScale = originalGravity;
     }
 }
