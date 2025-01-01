6a1cc2a639f371ca4e2019b80ca5919c9fb82df2
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-01 17:28:24 +0100

    coded pipe movement

--- Assets/PipeMovement.cs
+++ Assets/PipeMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PipeMovement : MonoBehaviour
 {
+    public float speed = 1.0f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,6 @@ public class PipeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * (speed * Time.deltaTime);
     }
 }
