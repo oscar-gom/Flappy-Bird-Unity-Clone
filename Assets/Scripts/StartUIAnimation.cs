e11f97ee649465b475d456aa6b31d6624560bbd0
Óscar Gómez <oscargomezsedas@gmail.com>
2025-01-03 14:05:07 +0100

    made tap finger animation

--- Assets/Scripts/StartUIAnimation.cs
+++ Assets/Scripts/StartUIAnimation.cs
@@ -1,16 +1,28 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartUIAnimation : MonoBehaviour
 {
+    private Image _tapImage;
+    private PlayerFlap _playerFlap;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _playerFlap = GameObject.Find("Player").GetComponent<PlayerFlap>();
+        _tapImage = GameObject.Find("Finger").GetComponent<Image>();
+        StartCoroutine(TapAnimation());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TapAnimation()
     {
-        
+        while (!_playerFlap.started)
+        {
+            yield return new WaitForSeconds(1f);
+            _tapImage.enabled = false;
+            yield return new WaitForSeconds(1f);
+            _tapImage.enabled = true;
+        }
     }
 }
